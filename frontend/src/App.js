b07f7bc7b0a4f9dedddce1d8f684789473ba9256
SyedQaimr <qaimkazmi514@gmail.com>
2023-11-25 22:08:37 +0500

    Frontend App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,23 @@ import React from "react";
 import Footer from "./component/layout/Footer/Footer";
 import Home from "./component/Home/Home";
 import ProductDetails from "./component/Product/ProductDetails";
-import Products from './component/Product/Products';
-import Search from './component/Product/Search'
+import Products from "./component/Product/Products";
+import Search from "./component/Product/Search";
+import LoginSignUp from "./component/User/LoginSignUp";
+import ForgotPassword from "./component/User/ForgotPassword";
+import ResetPassword from "./component/User/ResetPassword";
+import ProtectedRoute from "./component/Route/ProtectedRoute";
+import Profile from "./component/User/Profile";
+import store from "./store";
+import { loadUser } from "./actions/userAction";
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import UserOptions from "./component/layout/Header/UserOptions";
+import UpdateProfile from "./component/User/UpdateProfile";
+import UpdatePassword from "./component/User/UpdatePassword";
 
 function App() {
+  const { isAuthenticated, user } = useSelector((state) => state.user);
+
   React.useEffect(() => {
     WebFont.load({
       google: {
@@ -17,7 +30,7 @@ function App() {
       },
     });
 
-    // store.dispatch(loadUser());
+    store.dispatch(loadUser());
 
     // getStripeApiKey();
   }, []);
@@ -25,23 +38,26 @@ function App() {
   return (
     <Router>
       <Header />
+      {isAuthenticated && <UserOptions user={user} />}
       <Routes>
+        <Route path="/account" element={<Profile />} isAuthenticated />
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<ProductDetails />} />
         <Route path="/products" element={<Products />} />
-        <Route path="/search" element={<Search/>} />
-        <Route path="/products/:keyword" element={<Products/>} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/products/:keyword" element={<Products />} />
+        <Route path="/login" element={<LoginSignUp />} />
+        <Route path="/password/forgot" element={<ForgotPassword />} />
+        <Route path="/password/reset/:token" element={<ResetPassword />} />
+        <Route path="/me/update" element={<UpdateProfile />} />
+        <Route path="/password/update" element={<UpdatePassword />} />
       </Routes>
-      {/* 
 
-
-        <Route exact path="/contact" component={Contact} />
+      {/* <Route exact path="/contact" component={Contact} />
 
         <Route exact path="/about" component={About} />
+ */}
 
-        <ProtectedRoute exact path="/account" component={Profile} />
-
-        <ProtectedRoute exact path="/me/update" component={UpdateProfile} /> */}
       <Footer />
     </Router>
   );
