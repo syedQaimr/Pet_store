7b2edceb6ffd1cd149cd0d372d6a663433777607
SyedQaimr <qaimkazmi514@gmail.com>
2023-11-26 18:14:47 +0500

    Fix ProductCard

--- frontend/src/component/Home/ProductCard.js
+++ frontend/src/component/Home/ProductCard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import { Rating } from "@material-ui/lab"; 
-import ReactStars from "react-rating-stars-component"
+import { Rating } from "@material-ui/lab";
 
 
 const ProductCard = ({ product }) => {
@@ -12,17 +11,17 @@ const ProductCard = ({ product }) => {
     isHalf:true
   };
   return (
-    <Link className="productCard" to={`/product/${product._id}`}>
-      <img src={product.images[0].url} alt={product.name} />
-      <p>{product.name}</p>
+    <Link className="productCard" to={`/product/${product?._id}`}>
+      <img src={product?.images[0]?.url} alt={product?.name} />
+      <p>{product?.name}</p>
       <div>
-        <ReactStars {...options} />{" "}
+        <Rating {...options} />{" "}
         <span className="productCardSpan">
           {" "}
-          ({product.numOfReviews} Reviews)
+          ({product?.numOfReviews} Reviews)
         </span>
       </div>
-      <span>{`$${product.price}`}</span>
+      <span>{`$${product?.price}`}</span>
     </Link>
   );
 };
