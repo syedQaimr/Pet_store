8004800b86f96d3123376779b8a49f371f1b9cfe
SyedQaimr <qaimkazmi514@gmail.com>
2023-11-26 18:11:42 +0500

    Update PaymentRoute

--- backend/routes/paymentRoute.js
+++ backend/routes/paymentRoute.js
@@ -1,15 +1,15 @@
-// const express = require("express");
+const express = require("express");
 
 // Payments
-// const {
-//   processPayment,
-//   sendStripeApiKey,
-// } = require("../controllers/paymentController");
-// const router = express.Router();
-// const { isAuthenticatedUser } = require("../middleware/auth");
+const {
+  processPayment,
+  sendStripeApiKey,
+} = require("../controllers/paymentController");
+const router = express.Router();
+const { isAuthenticatedUser } = require("../middleware/auth");
 
-// router.route("/payment/process").post(isAuthenticatedUser, processPayment);
+router.route("/payment/process").post(isAuthenticatedUser, processPayment);
 
-// router.route("/stripeapikey").get(isAuthenticatedUser, sendStripeApiKey);
+router.route("/stripeapikey").get(isAuthenticatedUser, sendStripeApiKey);
 
-// module.exports = router;
+module.exports = router;
