d2b3558cee3209020c5805838d9e23e65e7b4ad7
SyedQaimr <qaimkazmi514@gmail.com>
2023-11-26 18:09:51 +0500

    Update productController

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,32 +2,32 @@ const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorhander");
 const catchAsyncErrors=require('../middleware/catchAsyncErrors');
 const ApiFeatures = require("../utils/apifeatures");
-
+const cloudinary = require("cloudinary");
 
 
 exports.createProduct = catchAsyncErrors( async (req, res, next) => {
-    // let images = [];
+    let images = [];
   
-    // if (typeof req.body.images === "string") {
-    //   images.push(req.body.images);
-    // } else {
-    //   images = req.body.images;
-    // }
+    if (typeof req.body.images === "string") {
+      images.push(req.body.images);
+    } else {
+      images = req.body.images;
+    }
   
-    // const imagesLinks = [];
+    const imagesLinks = [];
   
-    // for (let i = 0; i < images.length; i++) {
-    //   const result = await cloudinary.v2.uploader.upload(images[i], {
-    //     folder: "products",
-    //   });
+    for (let i = 0; i < images?.length; i++) {
+      const result = await cloudinary.v2.uploader.upload(images[i], {
+        folder: "products",
+      });
   
-    //   imagesLinks.push({
-    //     public_id: result.public_id,
-    //     url: result.secure_url,
-    //   });
-    // }
+      imagesLinks.push({
+        public_id: result.public_id,
+        url: result.secure_url,
+      });
+    }
   
-    // req.body.images = imagesLinks;
+    req.body.images = imagesLinks;
     req.body.user = req.user.id;
   
     const product = await Product.create(req.body);
